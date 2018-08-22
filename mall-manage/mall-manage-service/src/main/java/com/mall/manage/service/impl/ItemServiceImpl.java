456de193bcecc720689992dcb131528315a58697
gp6 <pfgao@ips.com>
2018-08-22 17:11:47 +0800

    后台----获取商品详情---增加缓存逻辑

--- mall-manage/mall-manage-service/src/main/java/com/mall/manage/service/impl/ItemServiceImpl.java
+++ mall-manage/mall-manage-service/src/main/java/com/mall/manage/service/impl/ItemServiceImpl.java
@@ -1,11 +1,17 @@
 package com.mall.manage.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mall.common.constant.Constants;
 import com.mall.common.utils.DateTimeUtil;
+import com.mall.common.utils.RedisUtil;
 import com.mall.manage.bean.Item;
 import com.mall.manage.bean.ItemDesc;
 import com.mall.manage.bean.ItemParam;
@@ -24,6 +30,20 @@ import com.mall.manage.vo.ItemVo;
  */
 @Service
 public class ItemServiceImpl implements ItemService {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ItemServiceImpl.class);
+	
+	private static final ObjectMapper OBJECTMAPPER = new ObjectMapper();
+
+	/**
+	 * 定义key的规则:项目名_模块名_业务名
+	 */
+	private static final String REDIS_KEY = "MANAGE_MALL_ITEM_DETAIL";
+	
+	private static final Integer REDIS_TIME = 60 * 60 * 24;
+	
+	@Autowired
+	private RedisUtil redisUtil;
 
 	@Autowired
 	private ItemDescService itemDescService;
@@ -60,10 +80,36 @@ public class ItemServiceImpl implements ItemService {
 		return Boolean.valueOf(
 				(countItem.intValue() == 1) && (countItemDesc.intValue() == 1) && (countItemParam.intValue() == 1));
 	}
-	
-	@Override
-	public ItemVo getItemInfoById(Long id){
-		return itemMapper.getItemInfoById(id);
-	}
 
+	@Override
+	public ItemVo getItemInfoById(Long id) {
+		
+		String key = REDIS_KEY + id;
+		
+		try {
+			// 从缓存中命中
+			String cacheData = redisUtil.get(key);
+			if (StringUtils.isNotEmpty(cacheData)) {
+				return OBJECTMAPPER.readValue(cacheData, ItemVo.class);
+			}
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		
+		ItemVo itemVo = itemMapper.getItemInfoById(id);
+		try {
+			if (null == itemVo) {
+				return null;
+			}
+			
+			// 将数据写入到缓存中
+			redisUtil.set(key, OBJECTMAPPER.writeValueAsString(itemVo), REDIS_TIME);
+			return itemVo;
+		} catch (JsonProcessingException e) {
+			LOGGER.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
