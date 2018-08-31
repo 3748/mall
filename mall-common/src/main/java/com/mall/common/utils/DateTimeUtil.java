d4bdc0308b50db1a1da01babef260efbd40e65bc
gp6 <pfgao@ips.com>
2018-08-31 17:37:38 +0800

    common----添加日志和jpa依赖

--- mall-common/src/main/java/com/mall/common/utils/DateTimeUtil.java
+++ mall-common/src/main/java/com/mall/common/utils/DateTimeUtil.java
@@ -5,23 +5,23 @@ import java.util.Date;
 
 /**
  * 日期时间工具类
- * 
+ *
  * @author gp6
  * @date 2018-07-09
  */
 public class DateTimeUtil {
 
-	private static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+    private static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
 
-	/**
-	 * 指定格式(YYYYMMDDHHMMSS)的当前时间 eg:20181231123456
-	 */
-	public static final Long CURRENTTIME = DateTimeUtil.getCurrentTime();
+    /**
+     * 指定格式(YYYYMMDDHHMMSS)的当前时间 eg:20181231123456
+     */
+    public static final Long CURRENTTIME = DateTimeUtil.getCurrentTime();
 
-	public static Long getCurrentTime() {
-		SimpleDateFormat formatter = new SimpleDateFormat(YYYYMMDDHHMMSS);
-		return Long.valueOf(formatter.format(new Date()));
-	}
+    public static Long getCurrentTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat(YYYYMMDDHHMMSS);
+        return Long.valueOf(formatter.format(new Date()));
+    }
 
 
 }
--- mall-common/src/main/java/com/mall/common/utils/HttpClientUtil.java
+++ mall-common/src/main/java/com/mall/common/utils/HttpClientUtil.java
@@ -29,134 +29,133 @@ import com.mall.common.http.client.HttpResult;
 
 /**
  * HttpClient工具类
- * 
+ *
  * @author gp6
  * @date 2018-08-21
  */
 @Service
 public class HttpClientUtil implements BeanFactoryAware {
 
-	/**
-	 *  如果Spring中有就注入,没有就忽略
-	 */
-	@Autowired(required = false)
-	private RequestConfig requestConfig;
+    /**
+     * 如果Spring中有就注入,没有就忽略
+     */
+    @Autowired(required = false)
+    private RequestConfig requestConfig;
 
-	/*
-	 * @Service是一个单例, 这种方法注入产生的closeableHttpClient也是一个单例, 
-	 * 而closeableHttpClient不能是单例,所以使用BeanFactory
-	 
-	 @Autowired 
-	 private CloseableHttpClient closeableHttpClient;
-	 */
-	private BeanFactory beanFactory;
+    /*
+     * @Service是一个单例, 这种方法注入产生的closeableHttpClient也是一个单例,
+     * 而closeableHttpClient不能是单例,所以使用BeanFactory
+     @Autowired
+     private CloseableHttpClient closeableHttpClient;
+     */
+    private BeanFactory beanFactory;
 
-	private CloseableHttpClient getHttpClient() {
-		// 通过Bean工厂获取bean，保证HttpClient对象是多例
-		return this.beanFactory.getBean(CloseableHttpClient.class);
-	}
+    private CloseableHttpClient getHttpClient() {
+        // 通过Bean工厂获取bean，保证HttpClient对象是多例
+        return this.beanFactory.getBean(CloseableHttpClient.class);
+    }
 
-	@Override
-	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-		// 初始化时,获取bean工厂,将bean工厂赋值给当前类中定义的beanFactory属性
-		this.beanFactory = beanFactory;
-	}
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        // 初始化时,获取bean工厂,将bean工厂赋值给当前类中定义的beanFactory属性
+        this.beanFactory = beanFactory;
+    }
 
-	/**
-	 * 指定GET请求，返回:null,请求失败，String数据，请求成功
-	 * 
-	 * @param url
-	 * @return
-	 * @throws ClientProtocolException
-	 * @throws IOException
-	 */
-	public String doGet(String url) throws ClientProtocolException, IOException {
-		// 创建http GET请求
-		HttpGet httpGet = new HttpGet(url);
-		httpGet.setConfig(requestConfig);
-		CloseableHttpResponse response = null;
-		try {
-			// 执行请求
-			response = getHttpClient().execute(httpGet);
-			// 判断返回状态是否为200
-			if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
-				return EntityUtils.toString(response.getEntity(), "UTF-8");
-			}
-		} finally {
-			if (response != null) {
-				response.close();
-			}
-		}
-		return null;
-	}
+    /**
+     * 指定GET请求，返回:null,请求失败，String数据，请求成功
+     *
+     * @param url
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     */
+    public String doGet(String url) throws ClientProtocolException, IOException {
+        // 创建http GET请求
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.setConfig(requestConfig);
+        CloseableHttpResponse response = null;
+        try {
+            // 执行请求
+            response = getHttpClient().execute(httpGet);
+            // 判断返回状态是否为200
+            if (response.getStatusLine().getStatusCode() == HttpStatus.OK.value()) {
+                return EntityUtils.toString(response.getEntity(), "UTF-8");
+            }
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * 带有参数的GET请求，返回:null,请求失败，String数据，请求成功
-	 * 
-	 * @param url
-	 * @param params
-	 * @return
-	 * @throws ClientProtocolException
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	public String doGet(String url, Map<String, String> params)
-			throws ClientProtocolException, IOException, URISyntaxException {
-		URIBuilder builder = new URIBuilder(url);
-		for (Map.Entry<String, String> entry : params.entrySet()) {
-			builder.setParameter(entry.getKey(), entry.getValue());
-		}
-		return this.doGet(builder.build().toString());
-	}
+    /**
+     * 带有参数的GET请求，返回:null,请求失败，String数据，请求成功
+     *
+     * @param url
+     * @param params
+     * @return
+     * @throws ClientProtocolException
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public String doGet(String url, Map<String, String> params)
+            throws ClientProtocolException, IOException, URISyntaxException {
+        URIBuilder builder = new URIBuilder(url);
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            builder.setParameter(entry.getKey(), entry.getValue());
+        }
+        return this.doGet(builder.build().toString());
+    }
 
-	/**
-	 * 带有参数的POST请求
-	 * 
-	 * @param url
-	 * @param params
-	 * @return
-	 * @throws ParseException
-	 * @throws IOException
-	 */
-	public HttpResult doPost(String url, Map<String, String> params) throws ParseException, IOException {
-		// 创建http POST请求
-		HttpPost httpPost = new HttpPost(url);
-		httpPost.setConfig(requestConfig);
-		if (null != params) {
-			// 设置post参
-			List<NameValuePair> parameters = new ArrayList<NameValuePair>(0);
-			for (Map.Entry<String, String> entry : params.entrySet()) {
-				parameters.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
-			}
-			// 构造一个form表单式的实体
-			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters);
-			// 将请求实体设置到httpPost对象中
-			httpPost.setEntity(formEntity);
-		}
+    /**
+     * 带有参数的POST请求
+     *
+     * @param url
+     * @param params
+     * @return
+     * @throws ParseException
+     * @throws IOException
+     */
+    public HttpResult doPost(String url, Map<String, String> params) throws ParseException, IOException {
+        // 创建http POST请求
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setConfig(requestConfig);
+        if (null != params) {
+            // 设置post参
+            List<NameValuePair> parameters = new ArrayList<NameValuePair>(0);
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                parameters.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+            }
+            // 构造一个form表单式的实体
+            UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters);
+            // 将请求实体设置到httpPost对象中
+            httpPost.setEntity(formEntity);
+        }
 
-		CloseableHttpResponse response = null;
-		try {
-			// 执行请求
-			response = getHttpClient().execute(httpPost);
-			return new HttpResult(response.getStatusLine().getStatusCode(),
-					EntityUtils.toString(response.getEntity(), "UTF-8"));
-		} finally {
-			if (response != null) {
-				response.close();
-			}
-		}
-	}
+        CloseableHttpResponse response = null;
+        try {
+            // 执行请求
+            response = getHttpClient().execute(httpPost);
+            return new HttpResult(response.getStatusLine().getStatusCode(),
+                    EntityUtils.toString(response.getEntity(), "UTF-8"));
+        } finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+    }
 
-	/**
-	 * 没有参数的POST请求
-	 * 
-	 * @param url
-	 * @return
-	 * @throws ParseException
-	 * @throws IOException
-	 */
-	public HttpResult doPost(String url) throws ParseException, IOException {
-		return this.doPost(url, null);
-	}
+    /**
+     * 没有参数的POST请求
+     *
+     * @param url
+     * @return
+     * @throws ParseException
+     * @throws IOException
+     */
+    public HttpResult doPost(String url) throws ParseException, IOException {
+        return this.doPost(url, null);
+    }
 
 }
