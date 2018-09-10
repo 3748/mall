fd6c15b06e0c0cded90e7cad770e6035ee00587c
gp6 <pfgao@ips.com>
2018-09-10 18:08:21 +0800

    rabbit----修改配置文件

--- itcast/itcast-rabbitmq/src/main/java/cn/itcast/rabbitmq/spring/Foo.java
+++ itcast/itcast-rabbitmq/src/main/java/cn/itcast/rabbitmq/spring/Foo.java
@@ -2,12 +2,17 @@ package cn.itcast.rabbitmq.spring;
 
 /**
  * 消费者
- * @author zhijun
  *
+ * @author gp6
+ * @date 2018/9/10
  */
 public class Foo {
 
-    //具体执行业务的方法
+    /**
+     * 具体执行业务的方法
+     *
+     * @param foo foo
+     */
     public void listen(String foo) {
         System.out.println("消费者： " + foo);
     }
--- itcast/itcast-rabbitmq/src/main/java/cn/itcast/rabbitmq/spring/SpringMain.java
+++ itcast/itcast-rabbitmq/src/main/java/cn/itcast/rabbitmq/spring/SpringMain.java
@@ -5,20 +5,21 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 
- * @author gp6
+ * 生产者
  *
+ * @author gp6
+ * @date 2018/9/10
  */
 public class SpringMain {
-	public static void main(final String... args) throws Exception {
-		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/rabbitmq-context.xml");
-		// RabbitMQ模板
-		RabbitTemplate template = ctx.getBean(RabbitTemplate.class);
-		// 发送消息
-		template.convertAndSend("Hello, world!");
-		// 休眠1秒
-		Thread.sleep(10 * 100);
-		// 容器销毁
-		ctx.destroy(); 
-	}
+    public static void main(final String... args) throws Exception {
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/rabbitmq-context.xml");
+        // RabbitMQ模板
+        RabbitTemplate rabbitTemplate = ctx.getBean(RabbitTemplate.class);
+        // 发送消息
+        rabbitTemplate.convertAndSend("Hello, world!");
+        // 休眠1秒
+        Thread.sleep(10 * 100);
+        // 容器销毁
+        ctx.destroy();
+    }
 }
