5545256936264868d37ca336107af9e8cf05798d
gp6 <pfgao@ips.com>
2018-09-29 18:02:09 +0800

    manage----加入注释

--- mall-manage/mall-manage-mapper/src/main/java/com/mall/manage/mapper/ItemMapper.java
+++ mall-manage/mall-manage-mapper/src/main/java/com/mall/manage/mapper/ItemMapper.java
@@ -12,8 +12,8 @@ public interface ItemMapper extends Mapper<Item> {
     /**
      * 根据Id获取商品信息
      *
-     * @param id
-     * @return
+     * @param id 商品id
+     * @return 商品信息
      */
     ItemVo getItemInfoById(Long id);
 }
